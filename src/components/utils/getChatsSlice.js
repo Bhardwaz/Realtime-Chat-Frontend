b2005d73369a0f23b33890663d7f334f6acb9701
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2024-03-11 12:07:48 +0530

    chat slice added

---
+++ src/components/utils/getChatsSlice.js
@@ -0,0 +1,37 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchChats = createAsyncThunk("fetchChats", async () => {
+  try {
+    const config = {
+      "Content-type": "application/json",
+    };
+    const { data } = await axios.get("/api/v1/chat", config);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const chatSlice = createSlice({
+  name: "chats",
+  initialState: {
+    isLoading: false,
+    data: null,
+    isError: false,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchChats.pending, (state) => {
+      state.isLoading = true;
+    }),
+      builder.addCase(fetchChats.fulfilled, (state, action) => {
+        (state.isLoading = false), (state.data = action.payload);
+      });
+    builder.addCase(fetchChats.rejected, (state, action) => {
+      console.log("Error", action.payload);
+      state.isError = true;
+    });
+  },
+});
+
+export default chatSlice.reducer;
