f3da2987aa0b4cd94b14875f200ba7b6b4953878
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2024-03-11 12:02:08 +0530

    component changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Login from './components/auth/Login.jsx'
-import Register from './components/auth/Register.jsx'
-import ChatsPage from './components/pages/ChatsPage.jsx'
-import ChatsHome from './components/pages/ChatsHome.jsx'
 import { Provider } from 'react-redux'
 import store from './components/utils/store.js'
+import SignUp from './components/auth/SignUp.jsx'
+import Home from './components/pages/Home.jsx'
+import { ChakraProvider } from '@chakra-ui/react'
+
 const router = createBrowserRouter([
   {
     path:'/',
@@ -16,27 +16,25 @@ const router = createBrowserRouter([
     children:[
       {
       path:"",
-      element:<Register/>
+      element:<SignUp/>
       },
       {
        path:"/login",
        element:<Login/>
       }, 
       {
-       path:"/chatspage",
-       element:< ChatsPage />
+       path:"/home",
+       element:< Home />
       }, 
-      {
-      path:"/homepage",
-      element: <ChatsHome/>
-      }
     ]
   }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <ChakraProvider>
   <Provider store={store}>
     <RouterProvider router={router} />
     <App />
-  </Provider>,
+  </Provider>
+  </ChakraProvider>
 )
