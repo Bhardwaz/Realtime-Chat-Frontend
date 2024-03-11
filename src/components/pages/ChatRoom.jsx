84bcde86c9b793b26288ed518385aabf443648bd
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2024-03-11 12:04:39 +0530

    chat room added

---
+++ src/components/pages/ChatRoom.jsx
@@ -0,0 +1,12 @@
+import { useSelector } from "react-redux"
+import { Box } from "@chakra-ui/react"
+
+function ChatRoom() {
+  return (
+    <div>
+      ChatRoom
+    </div>
+  )
+}
+
+export default ChatRoom
