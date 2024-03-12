9dac774e25cffbdf64a866bf1185e612f7342cc2
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2024-03-12 22:46:30 +0530

    messages rendered

--- src/components/misc/ScrollableChat.jsx
+++ src/components/misc/ScrollableChat.jsx
@@ -1,5 +1,5 @@
 import ScrollableFeed from "react-scrollable-feed"
-import { isLastMessage, isSameSender } from "../utils/chatLogics"
+import { isLastMessage, isSameSender, isSameSenderMargin, isSameUser } from "../utils/chatLogics"
 import { useSelector } from "react-redux"
 import { Avatar, Tooltip } from "@chakra-ui/react"
 
@@ -8,15 +8,15 @@ const ScrollableChat = ({ messages }) => {
   if(!user){
    user = JSON.parse(localStorage.getItem("loggedInUser"))
   }
-  console.log(messages, "messages");
-  console.log(user, "user");
+  console.log(messages);
   return (
     <ScrollableFeed>
         {
-        messages && messages.map((m, i) => {
-        return <div  style={{display:"flex"}} key={m._id}> 
+        messages && messages.map((m, i) => (
+         <div style={{ display:"flex" }} key={m._id}> 
         {
-          (isSameSender(messages, m, i, user._id)) || isLastMessage(messages, i, user._id ) && (
+          (isSameSender(messages, m, i, user._id) ||
+          isLastMessage(messages, i, user._id)) && (
             <Tooltip 
             label={m.sender.username}
             placement="bottom-start"
@@ -32,9 +32,23 @@ const ScrollableChat = ({ messages }) => {
             />
             </Tooltip>
           ) 
-        }          
+        }       
+        <span
+        style={{
+            backgroundColor: `${
+              m.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"
+            }`,
+            marginLeft: isSameSenderMargin(messages, m, i, user._id),
+            marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
+            borderRadius: "20px",
+            padding: "5px 15px",
+            maxWidth: "75%",
+          }}
+        >
+        { m.message }
+        </span>   
         </div>
-        })
+        ))
         }
     </ScrollableFeed>
   )
