0e316662d7f61f98ac52da6d21092663c4ae63c5
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2024-03-12 18:55:42 +0530

    chat loading UI added

--- src/components/misc/ChatLoading.jsx
+++ src/components/misc/ChatLoading.jsx
@@ -3,19 +3,13 @@ import {Stack, Skeleton} from "@chakra-ui/react"
 const ChatLoading = () => {
   return (
     <Stack>
-        <Skeleton height="45px" /> 
-        <Skeleton height="45px" /> 
-        <Skeleton height="45px" /> 
-        <Skeleton height="45px" /> 
-        <Skeleton height="45px" /> 
-        <Skeleton height="45px" /> 
-        <Skeleton height="45px" /> 
-        <Skeleton height="45px" /> 
-        <Skeleton height="45px" /> 
-        <Skeleton height="45px" /> 
-        <Skeleton height="45px" /> 
+      <Skeleton height="45px" />
+      <Skeleton height="45px" />
+      <Skeleton height="45px" />
+      <Skeleton height="45px" />
+      <Skeleton height="45px" />
     </Stack>
-  )
-}
+  );
+};
 
 export default ChatLoading
