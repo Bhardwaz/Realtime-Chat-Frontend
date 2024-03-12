c3b0c8de379300e657db1043d5318b5a1d67a97d
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2024-03-12 18:57:30 +0530

    chat room added

--- src/components/pages/ChatRoom.jsx
+++ src/components/pages/ChatRoom.jsx
@@ -1,11 +1,23 @@
 import { useSelector } from "react-redux"
 import { Box } from "@chakra-ui/react"
+import SingleChat from "./SingleChat"
 
-function ChatRoom() {
+function ChatRoom({ fetchAgain, setFetchAgain }) {
+  const selectedChat = useSelector(state => state.chatUser.selectedChat)
+
   return (
-    <div>
-      ChatRoom
-    </div>
+    <Box
+    display={{ base:selectedChat ? "flex" : "none", md:"flex" }}
+    alignItems='center'
+    flexDir='column'
+    p={3}
+    bg="white"
+    width={{ base:"100%", md:"68%" }}
+    borderRadius="lg"
+    borderWidth="1px"
+    >
+      <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+    </Box>
   )
 }
 
