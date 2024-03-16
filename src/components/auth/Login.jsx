4ce48882edc59ddf08aa876208eaf0e28b17a6b1
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2024-03-16 12:53:25 +0530

    bg color issue fixed

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -70,7 +70,7 @@ const Login = () => {
     }, [navigate])
 
   return (
-    <>
+    <div className="bg-black h-[100vh] w-[100vw]">
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div className="sm:mx-auto sm:w-full sm:max-w-sm">
     <img className="mx-auto h-10 w-auto" src="https://cdn-icons-png.flaticon.com/512/10948/10948696.png" alt=""/>
@@ -122,7 +122,7 @@ const Login = () => {
     </p>
   </div>
 </div>
-    </>
+    </div>
 
   )
 }
--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -88,7 +88,7 @@ export default function SignUp() {
     }, [navigate])
 
      return (
-      <>
+      <div className="bg-black h-[100vh] w-[100vw]">
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
@@ -279,7 +279,7 @@ export default function SignUp() {
             </p>
           </div>
         </div>
-      </>
+      </div>
     );
   }
   
